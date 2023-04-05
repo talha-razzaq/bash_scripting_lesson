b6d3469bbd53b51af6b6f6c503ab5c4b75f03cb5
Talha_Razzaq <105111642+talha-razzaq@users.noreply.github.com>
2023-04-05 06:11:06 +0500

    Add files via upload

---
+++ bash.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+# How to check the lines in  file
+
+line1=$(wc -l < $1)
+line2=$(wc -l < $2)
+line3=$(wc -l < $3)
+
+echo "There are $line1 lines in $1"
+echo "There are $line2 lines in $2"
+echo "There are $line3 lines in $3"
+
